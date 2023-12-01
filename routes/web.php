92f2c40ad70aea8dadc852a91c88939f3861dcb4
Atsuhiro-Sato <atuhiro0306@gmail.com>
2023-12-01 19:05:57 +0900

    カリキュラム08-4

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController; 
-
+use App\Http\Controllers\PostController;      //追加
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,5 +12,7 @@ use App\Http\Controllers\PostController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/posts', [PostController::class, 'index']);   
+Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::post('/posts', [PostController::class, 'store']);
+Route::get('/posts/{post}', [PostController::class ,'show']);
